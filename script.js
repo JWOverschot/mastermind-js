209d526f99f68581ccd380c3e11c4827ba30da2b
Jis van Overschot <99040091@mydavinci.nl>
2016-12-21 10:04:13 +0100

    + randomizer

---
+++ script.js
@@ -0,0 +1,7 @@
+var colors = ["red", "black", "white", "blue", "green", "yellow"];
+var code = [];
+
+for (var i = 0; i <= 3; i++) {
+	var random = Math.floor((Math.random() * 6) + 1);
+	code.push(random);
+}
