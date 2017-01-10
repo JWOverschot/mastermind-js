d7cf6d3db55c01f56ce81b5c6c69e34f0c8839fe
Jis van Overschot <99040091@mydavinci.nl>
2017-01-10 09:06:35 +0100

    + input that pushes strings into array

--- script.js
+++ script.js
@@ -8,3 +8,29 @@ function codeGenarator() {
 	}
 	console.log(code);
 }
+var codePlayer = [];
+function colorInArray(n) {
+	var pin = document.getElementById("pin-red");
+	
+	if (codePlayer.length < 4) {
+		if (n == "red") {
+			codePlayer.push("red");
+		}
+		else if (n == "black") {
+			codePlayer.push("black");
+		}
+		else if (n == "white") {
+			codePlayer.push("white");
+		}
+		else if (n == "blue") {
+			codePlayer.push("blue");
+		}
+		else if (n == "green") {
+			codePlayer.push("green");
+		}
+		else if (n == "yellow") {
+			codePlayer.push("yellow");
+		}
+	}
+	console.log(codePlayer);
+}
