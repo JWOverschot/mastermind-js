2ddeb26244b77c242186f9f63014b7a8fc1ab882
Jis van Overschot <99040091@mydavinci.nl>
2017-01-16 08:39:59 +0100

    no message

--- script.js
+++ script.js
@@ -4,9 +4,31 @@ var keyWhite = [];
 var keyBlack = [];
 var a = 12;
 
+function start() {
+	console.log("Don't cheat");
+	document.getElementById("win-lose").style.animationName = "slide-up";
+	codeGenarator();
+	setTimeout(function(){
+		document.getElementById("win-lose").style.display = "none";
+	}, 1990);
+}
+
+function up() {
+	document.getElementById("win-lose").style.animationName = "slide-up-little";
+	document.getElementsByTagName("i")[0].style.animationName = "slide-up-arrow";
+
+	setTimeout(function(){
+		document.getElementById("win-lose").style.top = "-90%";
+		document.getElementsByTagName("content")[0].style.display = "none";
+		document.getElementsByTagName("i")[0].style.top = "3%";
+		document.getElementsByTagName("i")[0].style.transform = "rotate(180deg)";
+		document.getElementById("win-lose").style.filter = "opacity(0.5)";
+	}, 1990);
+}
+
 function codeGenarator() {
 	var colors = ["red", "black", "white", "blue", "green", "yellow"];
-
+	code = [];
 	for (var i = 0; i <= 3; i++) {
 		var random = Math.floor((Math.random() * 5));
 		code.push(colors[random]);
@@ -61,12 +83,16 @@ function colorInArray(n) {
 		if (keyBlack.length == 4) {
 			document.getElementById("text").innerHTML = "YOU WIN!";
 			document.getElementById("win-lose").style.display = "block";
+			document.getElementById("win-lose").style.animationName = "slide-down";
+			document.getElementsByTagName("i")[0].style.display = "block";
 			console.log("You Win!!");
 		}
 		if (a == 1) {
 			if (keyBlack.length != 4) {
 				document.getElementById("text").innerHTML = "YOU LOSE!";
 				document.getElementById("win-lose").style.display = "block";
+				document.getElementById("win-lose").style.animationName = "slide-down";
+				document.getElementsByTagName("i")[0].style.display = "block";
 				console.log("You Lose!!");
 			}
 		}
